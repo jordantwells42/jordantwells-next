14ab4a97aa7968289afea98f5ea54cd4b14ec9a1
Jordan Wells <jordantwells@gmail.com>
2022-07-01 11:32:29 -0400

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -46,7 +46,7 @@ export default function Navbar() {
   };
 
   return (
-    <div className="transition-all ease-in-out duration-1000 rounded-full w-20 flex flex-col items-center justify-center fixed top-5 right-10 z-50">
+    <div className="transition-all ease-in-out duration-1000 rounded-full w-20 flex flex-col items-center justify-center fixed top-5 right-5 z-50">
       <div onClick={changeToggle} className="flex flex-col border-2 hover:cursor-pointer  rounded-full bg-blue-400 w-10 h-10 z-50">
 
       </div>
