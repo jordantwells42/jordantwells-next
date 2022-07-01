34ed7580bdee3c32e474647331c934dcdaf768c4
Jordan Wells <jordantwells@gmail.com>
2022-07-01 14:59:58 -0400

    Update cards.js

--- components/cards.js
+++ components/cards.js
@@ -20,12 +20,12 @@ export function Card(props){
     <Link href={props.link}>
       <div className="flex flex-col border-2 my-3">
         <div className="group w-full h-80">
-        <img className="transition-all ease-in-out duration-200 hover-none:brightness-[0.5] hover-hover:group-hover:brightness-[0.1]  object-cover w-full h-full " src={props.img} alt={props.alt} />
+        <img className="transition-all ease-in-out duration-200 hover-none:brightness-[0.65] hover-hover:group-hover:brightness-[0.1]  object-cover w-full h-full " src={props.img} alt={props.alt} />
         <div className="transition-all ease-in-out duration-200 hover-none:opacity-100 hover-hover group-hover:cursor-pointer opacity-10 group-hover:opacity-100 -translate-y-full h-full w-full text-center flex flex-col items-center justify-center px-3">
-          <h1 className="bold text-3xl">
+          <h1 className="font-bold text-3xl">
             {props.title}
           </h1>
-          <h2 className="bold text-xl">
+          <h2 className="italic text-blue-500 text-xl">
             {props.subtitle}
           </h2>
           <h2 className="bold text-lg mt-10">
