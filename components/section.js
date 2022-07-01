158eaf0a67debfdc91f7b4ee3b68895eff7ead9b
Jordan Wells <jordantwells@gmail.com>
2022-07-01 11:30:04 -0400

    Update section.js

--- components/section.js
+++ components/section.js
@@ -3,7 +3,7 @@ import React from "react";
 function Section(props) {
   return (
     <div className="py-5 text-white text-2xl w-3/4 items-center justify-center text-center">
-      <h1 id={props.name} className="text-4xl py-5">
+      <h1 id={props.name} className="text-4xl py-5 font-bold">
         {props.name}
       </h1>
       <div className="py-5">
