df1540e673341b12fa779477c78f74844f8383f9
Jordan Wells <jordantwells@gmail.com>
2022-07-01 11:52:12 -0400

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -41,7 +41,7 @@ export default function Footer() {
   };
 
   return (
-    <div className={toggleStyle.animate } + " overflow-hidden pb-10 flex flex-col items-center justify-center"}>
+    <div className={toggleStyle.animate + " overflow-hidden pb-10 flex flex-col items-center justify-center"}>
       <div onMouseEnter={changeToggle} className={toggleStyle.translateyinv + " transition-transform ease-out duration-200 mt-10 flex flex-col border-2 hover:cursor-pointer rounded-full bg-blue-400 w-10 h-10 z-50"}>
       </div>
       <div onMouseLeave={changeToggle} className={toggleStyle.translatey + " transition-transform ease-out duration-200 -mt-20 p-3 border-2 rounded-full bg-slate-800 flex flex-col items-center z-40"}>
