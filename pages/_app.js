e827116be3e3391df436834040b4921d707cd7b6
Jordan Wells <jordantwells@gmail.com>
2022-07-01 01:06:25 -0400

    removing package bloat

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,8 @@
 import "../styles/globals.css";
-import "jquery";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <link
-        rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-      ></link>
-
       <Component {...pageProps} />
-    </>
   );
 }
 
