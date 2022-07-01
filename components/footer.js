366b84dcb1b8024ab85d9f7713dcfe202ae5d682
Jordan Wells <jordantwells@gmail.com>
2022-07-01 13:16:46 -0400

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -42,9 +42,9 @@ export default function Footer() {
 
   return (
     <div className={toggleStyle.animate + " overflow-hidden pb-10 flex flex-col items-center justify-center"}>
-      <div onMouseEnter={changeToggle} className={toggleStyle.translateyinv + " transition-transform ease-out duration-200 mt-10 flex flex-col border-2 hover:cursor-pointer rounded-full bg-blue-400 w-10 h-10 z-50"}>
+      <div onMouseEnter={changeToggle} className={toggleStyle.translateyinv + " ease-out duration-200 mt-10 flex flex-col border-2 hover:cursor-pointer rounded-full bg-blue-400 w-10 h-10 z-50"}>
       </div>
-      <div onMouseLeave={changeToggle} className={toggleStyle.translatey + " transition-transform ease-out duration-200 -mt-20 p-3 border-2 rounded-full bg-slate-800 flex flex-col items-center z-40"}>
+      <div onMouseLeave={changeToggle} className={toggleStyle.translatey + " ease-out duration-200 -mt-20 p-3 border-2 rounded-full bg-slate-800 flex flex-col items-center z-40"}>
           <div className="flex flex-col items-center justify-center">
             <h1 className="text-white text-xl">Contact:</h1>
             <div className="flex flex-row justify-between">
@@ -52,7 +52,7 @@ export default function Footer() {
                 return (
                   <Link key={contact.name} href={contact.href}>
                     <a>
-                      <h1 className={toggleStyle.marginy + " transition-spacing ease-out duration-1000 text-center text-white text-lg hover:text-blue-300"}>{contact.name}</h1>
+                      <h1 className={toggleStyle.marginy + " ease-out duration-1000 text-center text-white text-lg hover:text-blue-300"}>{contact.name}</h1>
                       </a>
                   </Link>
                 )
