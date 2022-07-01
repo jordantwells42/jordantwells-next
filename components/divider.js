efb8628cea3de0e92f4d3ffea06d96cc5dfbc5ad
Jordan Wells <jordantwells@gmail.com>
2022-07-01 08:43:48 -0400

    Update divider.js

--- components/divider.js
+++ components/divider.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Divider(){
     return (
-      <div className="h-4 w-3/4 flex items-center justify-center border-white border-b-4 my-3">
+      <div className="h-4 w-3/4 flex items-center justify-center border-white border-b-2 my-3">
       </div>
     )
 }
