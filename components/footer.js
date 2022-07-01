8ccb69ce121b79c7e620219e11cf2fad368de640
Jordan Wells <jordantwells@gmail.com>
2022-07-01 19:13:31 -0400

    rotated navbar

--- components/footer.js
+++ components/footer.js
@@ -42,12 +42,12 @@ export default function Footer() {
 
   return (
     <div className={toggleStyle.animate + " overflow-hidden pb-10 flex flex-col items-center justify-center"}>
-      <div onMouseEnter={changeToggle} className={toggleStyle.translateyinv + " transition-transform ease-in-out duration-500 mt-10 flex flex-col justify-center items-center border-2 hover:cursor-pointer rounded-full bg-blue-400 w-10 h-10 z-50"}>
+      <div onMouseEnter={changeToggle} className={toggleStyle.translateyinv + " transition-transform ease-in-out duration-300 mt-10 flex flex-col justify-center items-center border-2 hover:cursor-pointer rounded-full bg-blue-400 w-14 h-14 z-50"}>
       <svg xmlns="http://www.w3.org/2000/svg" className="h-3/4 w-3/4 text-white text-center flex flex-row justify-center items-center" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
         <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
       </svg>
         </div>
-      <div onMouseLeave={changeToggle} className={toggleStyle.translatey + " transition-transform ease-in-out duration-500 -mt-20 p-3 border-2 rounded-full bg-slate-800 flex flex-col items-center z-40"}>
+      <div onMouseLeave={changeToggle} className={toggleStyle.translatey + " transition-transform ease-in-out duration-300 -mt-20 p-3 border-2 rounded-full bg-slate-800 flex flex-col items-center z-40"}>
           <div className="flex flex-col items-center justify-center">
             <h1 className="text-white text-xl">Contact:</h1>
             <div className="flex flex-row justify-between">
@@ -55,7 +55,7 @@ export default function Footer() {
                 return (
                   <Link key={contact.name} href={contact.href}>
                     <a>
-                      <h1 className={toggleStyle.marginy + " transition-spacing ease-in-out duration-1000 text-center text-white text-lg hover:text-blue-300"}>{contact.name}</h1>
+                      <h1 className={toggleStyle.marginy + " transition-spacing ease-in-out duration-500 text-center text-white text-lg hover:text-blue-300"}>{contact.name}</h1>
                       </a>
                   </Link>
                 )
--- components/navbar.js
+++ components/navbar.js
@@ -26,7 +26,7 @@ const links = [
 export default function Navbar() {
   let [toggle, setToggle] = useState(false);
   let [toggleStyle, setToggleStyle] = useState({
-    marginy: "-my-3",
+    marginy: "-mx-3",
     translatey: "-translate-y-80"
   })
 
@@ -34,27 +34,21 @@ export default function Navbar() {
     setToggle(p => !p);
     if (!toggle){
       setToggleStyle({
-        marginy: "my-0.5",
+        marginy: "mx-5",
         translatey: "translate-y-0"
       })
     } else {
       setToggleStyle({
-        marginy: "-my-3",
+        marginy: "-mx-3",
         translatey: "-translate-y-80"
       })
     }
   };
 
   return (
-    <div className="rounded-full w-20 flex flex-col items-center justify-center fixed top-5 right-5 z-50">
-      <div  onClick={changeToggle} className="flex flex-col items-center justify-center text-center border-2 hover:cursor-pointer  rounded-full bg-blue-400 w-14 h-14 z-50">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-3/4 w-3/4 text-white flex flex-row justify-center items-center" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </div>
-
-
-        <div className={toggleStyle.translatey + " p-3 border-2 rounded-full bg-slate-800 flex flex-col z-40"}>
+    <div className="rounded-full h-20 flex flex-row items-center justify-center fixed top-5 right-5 z-50">
+      
+      <div className={toggleStyle.translatey + " p-3 border-2 rounded-full bg-slate-800 flex flex-row z-40"}>
           {links.map(link => {
             return (
               <Link scroll={false} key={link.name} href={link.href}>
@@ -64,6 +58,14 @@ export default function Navbar() {
           })}
 
         </div>
+      <div  onClick={changeToggle} className="flex flex-col items-center justify-center text-center border-2 hover:cursor-pointer  rounded-full bg-blue-400 w-14 h-14 z-50">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-3/4 w-3/4 text-white flex flex-row justify-center items-center" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+        </svg>
+      </div>
+
+
+        
       
     </div>
   );
