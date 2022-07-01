1fa9f3bb590c619f0c7a3efc79977364e2029c84
Jordan Wells <jordantwells@gmail.com>
2022-07-01 15:03:09 -0400

    cleaned up project landing colors and spacing

--- components/projectlanding.js
+++ components/projectlanding.js
@@ -7,7 +7,7 @@ export default function ProjectLanding(props){
             <div className="-translate-y-2/3 h-full px-10">
                 <div className="flex flex-col justify-center items-center">
                     <h2 className="text-center text-6xl font-bold text-white">{props.title}</h2>
-                    <h1 className="text-center text-2xl font-bold italic text-blue-400">{props.subtitle}</h1>
+                    <h1 className="text-center text-2xl font-bold pt-2 italic text-blue-200">{props.subtitle}</h1>
 
                     <svg xmlns="http://www.w3.org/2000/svg" className="animate-bounce sticky mt-10 bottom-0 h-10 w-10 text-white z-50" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M16.707 10.293a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0l-6-6a1 1 0 111.414-1.414L9 14.586V3a1 1 0 012 0v11.586l4.293-4.293a1 1 0 011.414 0z" clipRule="evenodd" />
