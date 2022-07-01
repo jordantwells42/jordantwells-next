6277d078c82d71ae84ba0ad26d61566dd0c0d842
Jordan Wells <jordantwells@gmail.com>
2022-07-01 13:36:22 -0400

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -48,7 +48,7 @@ export default function Navbar() {
   return (
     <div className="transition-all ease-in-out duration-1000 rounded-full w-20 flex flex-col items-center justify-center fixed top-5 right-5 z-50">
       <div  onClick={changeToggle} className="flex flex-col border-2 hover:cursor-pointer  rounded-full bg-blue-400 w-10 h-10 z-50">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-1/2 w-1/2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-1/2 w-1/2 text-white flex flex-row justify-center items-center" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </div>
