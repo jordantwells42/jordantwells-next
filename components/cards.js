c9154aa73a1c28862e267f291f85b988d02ea357
Jordan Wells <jordantwells@gmail.com>
2022-07-01 08:37:16 -0400

    added border to cards

--- components/cards.js
+++ components/cards.js
@@ -18,7 +18,7 @@ export function ProjectCard(props) {
 export function Card(props){
   return (
     <Link href={props.link}>
-      <div className="flex flex-col" >
+      <div className="flex flex-col border-2" >
         <div className="group w-full h-80 py-1">
         <img className="transition-all ease-in-out duration-200 hover-none:brightness-[0.4] hover-hover:group-hover:brightness-[0.1]  object-cover w-full h-full " src={props.img} alt={props.alt} />
         <div className="transition-all ease-in-out duration-200 hover-none:opacity-100 hover-hover group-hover:cursor-pointer opacity-10 group-hover:opacity-100 -translate-y-full h-full w-full text-center flex flex-col items-center justify-center px-3">
